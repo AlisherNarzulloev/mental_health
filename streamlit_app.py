ec469c2717356f644668258d3985151e8b40768d
AlisherNarzulloev <alishernarzylloev@gmail.com>
2024-10-06 22:24:20 +0500

    update loaded model

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,18 @@ technology_usage = st.number_input('Technology Usage Hours:', min_value=0.0, max
 social_media_usage = st.number_input('Social Media Usage Hours:', min_value=0.0, max_value=24.0)
 gaming_hours = st.number_input('Gaming Hours:', min_value=0.0, max_value=24.0)
 screen_time_hours = st.number_input('Screen Time Hours:', min_value=0.0, max_value=24.0)
-stress_level = st.selectbox('Stress Level:', ['Low', 'Medium', 'High'])
 physical_activity = st.number_input('Physical Activity Hours:', min_value=0.0, max_value=24.0)
 
-input_data = [[age, technology_usage, social_media_usage, gaming_hours, screen_time_hours, stress_level, physical_activity]]
+stress_level = st.selectbox('Stress Level:', ['Low', 'Medium', 'High'])
+
+stress_mapping = {'Low': 0, 'Medium': 1, 'High': 2}
+stress_level_encoded = stress_mapping[stress_level]
+
+input_data = [[age, technology_usage, social_media_usage, gaming_hours, screen_time_hours, stress_level_encoded, physical_activity]]
 
 with open('catboost_model.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
 
 if st.button('Predict'):
     prediction = loaded_model.predict(input_data)
-
-
-
+    st.write(f'Mental Health Status Prediction: {prediction[0]}')
