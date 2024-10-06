b7661641ec92fa23eaf4a8f5fab3741c50217714
AlisherNarzulloev <alishernarzylloev@gmail.com>
2024-10-06 22:00:03 +0500

    edit file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 from catboost import CatBoostClassifier
 import pickle
 
-# Загрузка модели из файла
-
-# Ввод данных пользователя через интерфейс Streamlit
 st.title('Mental Health Prediction')
 
 age = st.number_input('Enter Age:', min_value=10, max_value=100)
@@ -15,14 +12,12 @@ screen_time_hours = st.number_input('Screen Time Hours:', min_value=0.0, max_val
 stress_level = st.selectbox('Stress Level:', ['Low', 'Medium', 'High'])
 physical_activity = st.number_input('Physical Activity Hours:', min_value=0.0, max_value=24.0)
 
-# Пример: формирование входных данных для предсказания
 input_data = [[age, technology_usage, social_media_usage, gaming_hours, screen_time_hours, stress_level, physical_activity]]
 
-# Предсказание
 if st.button('Predict'):
     with open('catboost_model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
-    prediction = loaded_model.predict(input_data)
+        loaded_model = pickle.load(f)
+        prediction = loaded_model.predict(input_data)
 
 
 
